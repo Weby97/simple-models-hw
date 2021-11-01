467ff713ecdc783a69405630faecca96b4d7abae
Brandon Ly <bl4720@g.rit.edu>
2021-11-01 13:17:10 -0700

    First commit Dog added!

---
+++ server/models/Dog.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+let DogModel = {};
+
+const DogSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    breed: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+      },
+  
+    age: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    
+    // I hope this is okay...if not I made an actual number version of createdDate below
+    createdDate: {
+      type: Date,
+      default: Date.now,
+    },
+    
+    // createdDate: {
+    //     type: Number,
+    //     default: Date.getTime(),
+    // },
+});
+
+DogSchema.statics.findByName = (name, callback) => {
+    const search = {
+      name,
+    };
+  
+    return DogModel.findOne(search, callback);
+};
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+
+// export our public properties
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
